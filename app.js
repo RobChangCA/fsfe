1505da936dbc178cea54be5c016e45e7fc91c94f
Rob Chang <robchang@Robs-MacBook-Pro.local>
2020-07-15 11:23:26 -0400

    fix

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+wow = { lol: "key" };
 app.get("/", (req, res) => {
-  res.send("Hello World!").status(418).set("x.lul", "bar");
+  res.send("Hello World!");
+});
+
+app.get("/test", (req, res) => {
+  res.set(wow);
+  res.status(418);
+  res.send("what this");
 });
 
 app.listen(port, () => console.log(`Example app is listening on port ${port}`));
